99a1311d678966568e3df0022bc5db2e95d44085
Madalin Bogdea <madalinbogdea@icloud.com>
2024-11-24 22:55:44 +0200

    add Header component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div className="m-auto max-w-md p-3">App</div>;
+  return (
+    <div className="m-auto max-w-md p-3">
+      <Header logo="todo" />
+    </div>
+  );
 };
 
 export default App;
