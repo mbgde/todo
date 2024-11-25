f1acf692cdbe6118da4af9f9ab73184a6157c87a
Madalin Bogdea <madalinbogdea@icloud.com>
2024-11-25 22:53:35 +0200

    add TaskList component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import TaskList from "./components/TaskList";
 
 const App = () => {
   return (
     <div className="m-auto max-w-md p-3">
       <Header logo="todo" />
+      <TaskList />
     </div>
   );
 };
