3cecff3bc83d64e09bc1b7987c8b2a2daf5279e5
Madalin Bogdea <madalinbogdea@icloud.com>
2024-11-25 21:49:56 +0200

    create DeleteButton component

---
+++ src/components/ui/DeleteButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TbTrash } from "react-icons/tb";
+
+const DeleteButton = () => {
+  return (
+    <div>
+      <TbTrash />
+    </div>
+  );
+};
+
+export default DeleteButton;
