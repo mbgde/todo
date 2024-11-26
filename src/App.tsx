6a7af1eca41dca18ea75ae50967729a0dd66d16b
Madalin Bogdea <madalinbogdea@icloud.com>
2024-11-26 11:41:25 +0200

    add CreateTaskButton component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import TaskList from "./components/TaskList";
+import CreateTaskButton from "./components/ui/CreateTaskButton";
 
 const App = () => {
   return (
     <div className="m-auto max-w-md p-3">
       <Header logo="todo" />
       <TaskList />
+      <CreateTaskButton />
     </div>
   );
 };
