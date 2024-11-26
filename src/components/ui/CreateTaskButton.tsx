d257cc249931562cea62efac43d35f807ac14906
Madalin Bogdea <madalinbogdea@icloud.com>
2024-11-26 11:40:35 +0200

    create CreateTaskButton component

---
+++ src/components/ui/CreateTaskButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateTaskButton = () => {
+  return (
+    <button className="bg-royalBlue w-full rounded-lg py-[1px] text-white">
+      create task
+    </button>
+  );
+};
+
+export default CreateTaskButton;
